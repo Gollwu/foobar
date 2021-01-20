db95d0c8d0ddd500f693e7abb72dfbd92ed514bc
ElJefe <jean-francois.aubry@nkt.dev>
2021-01-20 17:45:02 +0100

    Backend logic

--- foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
+++ foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
@@ -6,7 +6,30 @@ import org.springframework.stereotype.Component
 class FooBarQuixService {
 
     fun convertNumber(inputNumber: Int): String {
-        return "test"
-    }
+    	val chars: CharArray = inputNumber.toString().toCharArray();
 
+    	// Case when none of the rules apply then send back the number as String
+    	if(inputNumber%3 != 0 && inputNumber%5 != 0 && !chars.contains('3') && !chars.contains('5') && !chars.contains('7')) {
+    		return inputNumber.toString();
+    	}
+
+    	val sb = StringBuilder();
+    	if(inputNumber%3 == 0 ) {
+    		sb.append("Foo");
+    	}
+    	if(inputNumber%5 == 0 ) {
+			sb.append("Bar");
+    	}
+
+    	for(c in chars) {
+    		if (c == '3') {
+    			sb.append("Foo");
+			} else if (c == '5') {
+    			sb.append("Bar");
+    		} else if (c == '7') {
+    			sb.append("Quix");
+    		}
+    	}
+        return sb.toString();
+    }   
 }
